b8f0eb0ae63458e610a7d1b20187313a6b53e4b3
Sławomir Kurczewski <kurczewski7@gmail.com>
2023-08-24 19:12:42 +0200

    Automatic title in different language.

---
+++ testownik/Controllers/ResultsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ResultsViewController.swift
+//  
+//
+//  Created by Sławek K on 24/08/2023.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = Setup.placeHolderRatings
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var Label: UILabel!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
